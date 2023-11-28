b05154782f65d2e970dcb2449c243021f7466dab
Yashoda <yashodalakshmi999@gmail.com>
2023-11-28 17:01:34 +0530

    12 day

---
+++ Day-12-CSS-Column-Span-Property-Lab/script.js
@@ -0,0 +1,5 @@
+    // JavaScript function to change column count
+    function changeColumnCount(count) {
+      const container = document.getElementById('container');
+      container.style.columnCount = count;
+    }
