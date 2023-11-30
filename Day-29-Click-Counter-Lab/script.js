2ba822062d56aa65ad30603b1a4863b2835744f6
Yashoda <yashodalakshmi999@gmail.com>
2023-11-30 16:09:50 +0530

    29 - Click Counter Lab

---
+++ Day-29-Click-Counter-Lab/script.js
@@ -0,0 +1,8 @@
+// Add the click event listener here
+
+function increment(){
+    var counterDisplay = document.getElementById("count-display");
+    var currentCount = parseInt(counterDisplay.innerText, 10) || 0;
+    counterDisplay.innerText = currentCount + 1;
+}
+    
